5bc1f83067d34ceb74c43c1e38416c87b991fe1b
Wilfred <Wilfred@192.168.1.4>
2019-11-05 10:17:37 +0530

    test output

---
+++ demoebay/test-outout/report.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/Wilfred/git/EbaySample/demoebay/src/main/resources/Feature/Homepage.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Ebay Homepage URL",
+  "description": "",
+  "id": "ebay-homepage-url",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Open Ebay homepage test",
+  "description": "",
+  "id": "ebay-homepage-url;open-ebay-homepage-test",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 7,
+  "name": "I am in the Ebay Homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I view the Homepage",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter \u0027Books\u0027 in search options",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I Click on the first product from the dropdown",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
